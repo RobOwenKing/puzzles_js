3716edfa4f3e5af545ec93a179a3d0ea4b9a8a26
Robert King <robert.o.king@hotmail.co.uk>
2021-07-06 17:39:02 +0200

    Initialised: Project with empty main.js and template index.html

---
+++ main.js
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Puzzles JS</title>
+    <meta name="description" content="A web app to display a range of pencil puzzles using JS and HTML Canvas.">
+    <meta name="author" content="RobOwenKing">
+  </head>
+
+  <body>
+    <script src="main.js"></script>
+  </body>
+</html>
